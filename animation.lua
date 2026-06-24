c3f15653b305ef60ac6235e774d53da22284fef1
Xaxeric <xaxeric.dev@gmail.com>
2026-06-24 12:17:16 +0700

    tuning animations

--- animation.lua
+++ animation.lua
@@ -40,13 +40,13 @@ hl.config({
 
 -- https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/#curves 
 hl.curve( "spring", { type = "spring", mass = 1, stiffness = 70, dampening = 10 } )
-hl.curve( "bounce", { type = "spring", mass = 1, stiffness = 50, dampening = 8 } )
+hl.curve( "bounce", { type = "spring", mass = 1, stiffness = 40, dampening = 8 } )
 hl.curve( "smooth_ease", { type = "bezier", points = { {0.25, 0.1}, {0.25, 1.0} } } ) -- Fallback
 hl.animation({ leaf = "global", enabled = true, speed = 4, bezier = "smooth_ease" })
 hl.animation({ leaf = "windows", enabled = true, speed = 4, spring = "spring", style = "popin 80%" })
 hl.animation({ leaf = "workspaces", enabled = true, speed = 5, spring = "bounce", style = "slidefade" })
 
-hl.animation({ leaf = "fade", enabled = true, speed = 6, bezier = "smooth_ease" })
+hl.animation({ leaf = "fade", enabled = true, speed = 3, bezier = "smooth_ease" })
 hl.animation({ leaf = "fadeSwitch", enabled = true, speed = 4, bezier = "smooth_ease" })
 hl.animation({ leaf = "fadeShadow", enabled = true, speed = 4, bezier = "smooth_ease" })
 hl.animation({ leaf = "fadeDim", enabled = true, speed = 3, bezier = "smooth_ease" })
