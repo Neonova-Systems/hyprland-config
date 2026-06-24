e5e2baed70befe8459b5430eed037c070988a2e5
Xaxeric <xaxeric.dev@gmail.com>
2026-06-24 12:38:35 +0700

    fix: typo

--- animation.lua
+++ animation.lua
@@ -42,10 +42,10 @@ hl.config({
 hl.curve( "spring", { type = "spring", mass = 1, stiffness = 70, dampening = 14 } )
 hl.curve( "bounce", { type = "spring", mass = 1, stiffness = 100, dampening = 20 } )
 hl.curve( "smooth_ease", { type = "bezier", points = { {0.25, 0.1}, {0.25, 1.0} } } ) -- Fallback
-hl.curve( "easeInOut", { type = "bezier", points = { {.37, 0}, {0.63, 1} } } ) -- Fallback
+hl.curve( "easeInOut", { type = "bezier", points = { {0.37, 0}, {0.63, 1} } } ) -- Fallback
 hl.animation({ leaf = "global", enabled = true, speed = 4, bezier = "smooth_ease" })
 hl.animation({ leaf = "windowsIn", enabled = true, speed = 4, spring = "spring", style = "popin 80%" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 3, spring = "easeInOut", style = "gnomed" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 3, bezier = "easeInOut", style = "gnomed" })
 hl.animation({ leaf = "workspaces", enabled = true, speed = 3, spring = "bounce", style = "slidefade" })
 
 hl.animation({ leaf = "fade", enabled = true, speed = 3, bezier = "smooth_ease" })
