cb043e79218464c51e605da8781b931975a0eedf
Xaxeric <xaxeric.dev@gmail.com>
2026-06-24 18:58:16 +0700

    tuning animations

--- animation.lua
+++ animation.lua
@@ -45,7 +45,7 @@ hl.curve( "smooth_ease", { type = "bezier", points = { {0.25, 0.1}, {0.25, 1.0} 
 hl.curve( "easeInOut", { type = "bezier", points = { {0.37, 0}, {0.63, 1} } } )
 hl.animation({ leaf = "global", enabled = true, speed = 4, bezier = "smooth_ease" })
 hl.animation({ leaf = "windowsIn", enabled = true, speed = 4, spring = "spring", style = "popin 80%" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 10, bezier = "easeInOut"})
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 5, bezier = "easeInOut"})
 hl.animation({ leaf = "workspaces", enabled = true, speed = 3, spring = "bounce", style = "slidefade" })
 
 hl.animation({ leaf = "fade", enabled = true, speed = 5, bezier = "smooth_ease" })
